46d6fd9299e40daf44ae916e49602dce83a077ae
EvanFarrell7Stu <g00326940@atu.ie>
2024-12-13 02:34:45 +0000

    fingerscrossed

--- EFpage.js
+++ EFpage.js
@@ -1,44 +1,117 @@
 
-function checker(rndm){
-   
-    var  i;
-    
-    console.log(rndm);
-    
+var x=1;
+var y=1;
+var z=1;
 
-    var n1=document.getElementById("myNum1").value;
-    var num1=parseInt(n1);
-    if(num1===rndm){
-        document.getElementById("result2").innerHTML=+num;
+function p7p(){
 
+    var pic1=document.getElementById("p7p");
+    x++;
+
+    if(x%2==1){
+    pic1.src="pixel-7-pro.avif";
+    pic1.height= "300px";
+     pic1.width= auto;
     }
-    if (num1<rndm){
-        document.getElementById("Success").style.display="none";
-        document.getElementById("Too low").style.background="white";
-        document.getElementById("Too low").style.display="block";
-        document.getElementById("Too high").style.display="none";
-    } 
-    if (num1>rndm){
-        document.getElementById("Success").style.display="none";
-        document.getElementById("Too high").style.background="white";
-        document.getElementById("Too low").style.display="none";
-        document.getElementById("Too high").style.display="block";
+    else{
+    pic1.src="pixel-7-pro-back.avif";
+    pic1.height= "300px";
+     pic1.width= auto;
     }
+}
+
+function p8(){
+    var pic2=document.getElementById("p8");
+    y++
+
+    if(y%2==1){
+    pic2.src="pixel-8.avif";
+    pic2.height= "300px";
+    pic2.width= auto;
+    }
+    else{
+    pic2.src="pixel-8-front.avif";
+    pic2.height= "300px";
+    pic2.width= auto;
+    }
+}
+
+function p8p(){
+    var pic3=document.getElementById("p8p");
+    z++
+
+    if(z%2==1){
+    pic3.src="pixel-8-pro.avif";
+    pic3.height= "300px";
+    pic3.width= auto;
+    }
+    else{
+    pic3.src="pixel-8-pro-back.avif";
+    pic3.height= "300px";
+    pic3.width= auto;
+    }
+}
+
+
+
+
+var price = [499, 315, 273];
+var a = [38, 23 , 12];
+var num=0;
+var total=0;
+
+
+
+
+
+function addCart1(){
+    total+=price[0];
+    document.getElementById("Total").innerHTML=total;
+    a[0]-=1;
+    numCart();
+    stock1();
 
 }
 
-function stockgen(){
-    var x, rndm;
-    x=Math.random()*50;
-    rndm = Math.ceil(x);
-
-    return rndm;
-
+function addCart2(){
+    total+=price[1];
+    document.getElementById("Total").innerHTML=total;
+    a[1]-=1;
+    numCart();
+    stock2();   
 }
 
-function stock3(){
+function addCart3(){    
+    total+=price[2];
+    document.getElementById("Total").innerHTML=total;
+    a[2]-=1;
+    numCart();
+    stock3();
+}
+
+function numCart(){
+    num+=1;
+    document.getElementById("NumCart").innerHTML=num;
+ 
+}
+
+function stock1(){ 
     var x;
-    x=stockgen();
+    x=a[0];
     console.log(x);
-    document.getElementById("stock3").innerHTML=x;
+    document.getElementById("stock1").innerHTML=x+' in stock';
 }
+function stock2(){ 
+    var x;
+    x=a[1];
+    console.log(x);
+    document.getElementById("stock2").innerHTML=x+' in stock';
+}
+function stock3(){ 
+    var x;
+    x=a[2];
+    console.log(x);
+    document.getElementById("stock3").innerHTML=x+' in stock';
+}
+
+
